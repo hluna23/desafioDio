e85ab9b4b7db2e82298e8a281e69b7b09cdf76ee
Henry Luna <lunaretornado2@gmail.com>
2024-06-03 19:25:11 -0300

    desafio super heroi, Dio

---
+++ desafioDio.js
@@ -0,0 +1,25 @@
+
+
+
+const heroe = [
+  "Superman", 
+  "Wonder Woman",
+  "Iron Man",
+  "Batman",
+  "Spider-Man"
+]
+
+const xp =[
+  1000,
+  2000,
+  5000,
+  7000,
+  8000,
+  9000,
+  10000
+]
+
+
+
+
+console.log('seu supero' +  heroe[1] + ' ' + 'tem xp' + ' ' + xp[3] + ' ' + 'ele é  Ouro')
